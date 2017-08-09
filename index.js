a119f9bdd1edbbcdedc09d1f398afa09be40c999
glenn.dufresne <glenn.dufresne@accenture.com>
2017-08-09 16:42:28 -0500

    initial commit of sanitized error module

---
+++ index.js
@@ -0,0 +1,10 @@
+
+var config = {};
+
+module.exports = function(config) {
+    // Initialize opts in case it isn't passed in
+    global.config = config || {};
+
+    var errorHandler = require('./lib/errorHandler');
+    return errorHandler;
+}
